596bcb4af218d4053e625a454bafa1adfc008af4
Thomas Bertolani <thomas.b01@yahoo.com>
2024-12-28 18:16:00 +0100

    fixes db connection

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 
 load_dotenv()
 DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS= os.getenv("DB_PASS")
 
@@ -16,6 +17,7 @@ app = FastAPI()
 def query_db(query):
     conn = psycopg2.connect(
             host=DB_HOST,
+            dbname=DB_NAME,
             user=DB_USER,
             password=DB_PASS,
         )
